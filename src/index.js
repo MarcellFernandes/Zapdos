ca48c04184100862c3cd42fb98040d28fa599c6f
Gabriel Seixas <salesseixasgabriel@gmail.com>
2021-06-30 21:06:27 -0300

    Class 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(bodyParser.json())
 
+
 /*
  * list   - GET
  * get    - GET
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
 
 // GET http://localhost:3000/api/contacts
 app.get('/api/contacts', (req, res) => {
-  req.query
+  // req.query
   return res.json(contacts)
 })
 
@@ -36,15 +37,60 @@ app.get('/api/contacts', (req, res) => {
  */
 // POST http://localhost:3000/api/contacts
 app.post('/api/contacts', (req, res) => {
-  const contact = req.body  
-  contacts.push(contact)
+  // {
+  //   body: {
+  //     contact: {
+  //       name: 'lalala',
+  //       email: 'laala@la.com'
+  //     }
+  //   }
+  // }
+
+  // const ususarios = ['marcell', 'gabriel'] // length = 2
+  // usuarios.push('victor')
+  // usuarios = ['marcell', 'gabriel', 'victor']
+  // usuarios.pop()
+
+  const { contact } = req.body
+  const lastPosition = contacts.lenght - 1;
+  const lastContact = contacts[lastPosition]
+
+  if ('id' in lastContact) {
+    const newContact = { id: lastContact.id + 1, ...contact }
+    contacts.push(newContact)
+  } else {
+    const newContact = { id: 1, ...contact }
+    contacts.push(newContact)
+  }
   
   return res.json(contact)
 })
 
 /**
  * Criar uma rota get que pegue por id
+ * GET http://localhost:3000/api/contacts?id=2&name=gabriel
  */
+app.get('/api/contacts/single-contact', (req, res) => {
+  // { id: 2 }
+  const { id } = req.query
+  // const user = [{ id: 1, name: 'gabriel' }, { id: 2, name: 'marcell' }]
+  // const products = [{ user_id: 1, name: 'ps4' }, {user_id: 2, name: 'xbox'}...]
+
+  // map
+  // reduce
+  // filter
+
+  // const usersWithProducts = user.map((user) => {
+  //   user.product = product.filter(product => product.user_id === user.id)
+  //   return user
+  // })
+
+  const contactFound = contacts.filter(contact => {
+    return contact.id === id
+  })
+
+  return res.json(contactFound)
+})
 
 app.listen(3000, () => {
   console.log('App is running...')
